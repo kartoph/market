0214f1522e8da20abc4f8b422bb193d56d1153a7
kartoph <24b_pav@liceum.ztu.edu.ua>
2025-04-28 11:14:02 +0300

    Refactor HTML structure and CSS styles for improved layout; add dynamic item listings with pricing and discount details; update JavaScript to fetch and display items from JSON.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,36 @@
-// try to connect to the file script.js
-console.log("script.js loaded");
+console.log('Підключено файл скриптів market.js');
 
-let itemsArray = [
-    'Газоноскосарка 43',
-    'Електричний тример 110',
-    'Акамуляторна газонокосарка 32',
-    'Акумуляторний оприскувач 12 N',
-];
 
-let itemsDiv = document.getElementById("items");
-// check if the element is found
-if (itemsDiv) {
-    itemsArray.forEach((item, index) => {
-        itemsDiv.innerHTML += `<div class="item">
-        <h2>Товар №${index + 1} з ${itemsArray.length}</h2>
-        <p>${item}</p>
-        </div>`; 
+fetch('js/items.json') 
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Помилка завантаження JSON');
+        }
+        return response.json();
+    })
+    .then(data => {
+        data.forEach(item => {
+       
+            let itemElement = document.getElementById(item.id);
+            if (itemElement) {
+               
+                let titleElement = itemElement.querySelector(".item-title");
+                if (titleElement) {
+                    titleElement.textContent = item.name;
+                }
+
+                
+                let imgElement = itemElement.querySelector("img");
+                if (imgElement) {
+                    imgElement.src = item.image; 
+                    imgElement.alt = item.name; 
+                }
+            }
+        });
+    })
+    .catch(error => {
+        console.error('Помилка:', error);
     });
-} else {
-    console.log("Element with id 'items' not found.");
-}
-
 itemsArray = itemsArray.sort();
 itemsArray.forEach((item, index) => {
     console.log(item);
